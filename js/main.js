093ddb40b945e174ac067251f7be119e260faad6
musnit <musnit@gmail.com>
2014-10-30 17:38:31 +0200

    Refactor into proper objects

--- js/main.js
+++ js/main.js
@@ -1,58 +1,82 @@
-
 paper.install(window);
-window.onload = function() {
-    paper.setup('myCanvas');
-    Game = {};
-    Game.things = [];
-    Game.obstacles = [];
-    Game.timePlayed = 0;
-    Game.mouseClicks = [];
 
-    Game.moveThings = function(event){
-        Game.things.forEach(function(thing){
+function Game(sketch){
+    this.sketch = sketch;
+    this.things = [];
+    this.obstacles = [];
+    this.timePlayed = 0;
+    this.mouseClicks = [];
+
+    this.spawnCooldown = 0.5;
+
+    this.player = new Player(this);
+    this.player.jumping = false;
+    this.player.startingX = 100; 
+    this.player.startingY = 200; 
+    this.player.startingPosition = new Point(this.player.startingX, this.player.startingY); 
+    this.player.placedSymbol = this.player.symbol.place(this.player.startingPosition);
+    this.player.startingX2 = 100; 
+    this.player.startingY2 = 400; 
+    this.player.startingPosition2 = new Point(this.player.startingX2, this.player.startingY2); 
+    this.player.placedSymbol2 = this.player.symbol2.place(this.player.startingPosition2);
+    this.things.push(this.player);
+}
+
+Game.prototype = {
+    constructor: Game,
+    setupSketch: function(sketch){
+        this.sketch = sketch;
+    },
+    moveThings: function(event){
+        this.things.forEach(function(thing){
             thing.move(event)
         });
-    }
-
-    Game.spawnThings = function(event){
-        if (Game.spawnCooldown < 0) {
-            obstacle = new Game.Obstacle();
+    },
+    spawnThings: function(event){
+        if (this.spawnCooldown < 0) {
+            obstacle = new Obstacle();
             obstacle.placedSymbol = obstacle.symbol.place(obstacle.startingPosition);
             obstacle.placedSymbol2 = obstacle.symbol2.place(obstacle.startingPosition2);
             obstacle.spawnTime = event.time;
-            Game.obstacles.push(obstacle);
-            Game.things.push(obstacle);
-            Game.spawnCooldown = 1.5;
+            this.obstacles.push(obstacle);
+            this.things.push(obstacle);
+            this.spawnCooldown = 1.5;
         }
         else {
-            Game.spawnCooldown = Game.spawnCooldown - event.delta;
-        }
+            this.spawnCooldown = this.spawnCooldown - event.delta;
+        }    
     }
+}
 
-    Game.player = {};
-    Game.player.path = new Path.Rectangle([0, 0], [40, 50]);
-    Game.player.path2 = new Path.Rectangle([0, 0], [40, 300]);
-    Game.player.path.fillColor =  tinycolor("hsv(275, 100%, 100%)").toHexString()
-    Game.player.path2.fillColor = tinycolor("hsv(275, 100%, 100%)").toHexString()
-    Game.player.symbol = new Symbol(Game.player.path);
-    Game.player.symbol2 = new Symbol(Game.player.path2);
-    Game.player.move = function(event){
-        if (Game.player.startJump){
-            Game.player.startJump = false;
-            Game.player.jumping = true;
-            Game.player.jumpStartTime = event.time;
-            Game.mouseClicks.push(event.time);
+function Player(game){
+    this.path = new Path.Rectangle([0, 0], [40, 50]);
+    this.path2 = new Path.Rectangle([0, 0], [40, 300]);
+    this.path.fillColor =  tinycolor("hsv(275, 100%, 100%)").toHexString()
+    this.path2.fillColor = tinycolor("hsv(275, 100%, 100%)").toHexString()
+    this.symbol = new Symbol(this.path);
+    this.symbol2 = new Symbol(this.path2);
+    this.game = game;
+}
+
+Player.prototype = {
+    constructor: Player,
+    move: function(event){
+        if (this.startJump){
+            this.startJump = false;
+            this.jumping = true;
+            this.jumpStartTime = event.time;
+            this.game.mouseClicks.push(event.time);
         }
-        if (Game.player.jumping){
+        if (this.jumping){
             var jumpingTime = event.time - this.jumpStartTime;
             this.newY = this.startingY - this.calculateJumpHeight(jumpingTime);
             this.placedSymbol.position = new Point(this.startingX, this.newY);
             this.newCol = this.colorize(this.calculateJumpHeight(jumpingTime));
             this.placedSymbol2.symbol._definition.fillColor = this.newCol;
         }
-    };
+    },
     //0 < y < 50 < y < 78
-    Game.player.calculateJumpHeight = function(t){
+    calculateJumpHeight: function(t){
         var v0 = 370;
         var a = -780;
         var r = v0*t + a*t*t/2
@@ -63,10 +87,10 @@ window.onload = function() {
         else{
             return (v0*t + a*t*t/2);
         }
-    };
+    },
     // 250 < y < 300 < y < 328
     //TODO: Add blending/transparency to player and block so a miss is visualized nicer
-    Game.player.colorize = function(height){
+    colorize: function(height){
         var newCol;
         if (height<50){
             newCol = height+250;
@@ -75,52 +99,48 @@ window.onload = function() {
             newCol = 328;
         }
         return tinycolor("hsv(" + newCol + ", 100%, 100%)").toHexString()
-    };
+    }
+}
 
-    Game.Obstacle = function(){};
-    Game.Obstacle.prototype.path = new Path.Rectangle([0, 0], [40, 50]);
-    Game.Obstacle.prototype.path2 = new Path.Rectangle([0, 0], [40, 300]);
-    Game.Obstacle.prototype.path.fillColor =  tinycolor("hsv(325, 100%, 100%)").toHexString()
-    Game.Obstacle.prototype.path2.fillColor =  tinycolor("hsv(325, 100%, 100%)").toHexString()
-    Game.Obstacle.prototype.symbol = new Symbol(Game.Obstacle.prototype.path);
-    Game.Obstacle.prototype.symbol2 = new Symbol(Game.Obstacle.prototype.path2);
-    Game.Obstacle.prototype.startingX = 600; 
-    Game.Obstacle.prototype.startingY = 200; 
-    Game.Obstacle.prototype.startingY2 = 400; 
-    Game.Obstacle.prototype.startingPosition = new Point(Game.Obstacle.prototype.startingX, Game.Obstacle.prototype.startingY); 
-    Game.Obstacle.prototype.startingPosition2 = new Point(Game.Obstacle.prototype.startingX, Game.Obstacle.prototype.startingY2); 
-    Game.Obstacle.prototype.move = function(event){
+function Obstacle(){
+    this.path = new Path.Rectangle([0, 0], [40, 50]),
+    this.path2 = new Path.Rectangle([0, 0], [40, 300]),
+    this.path.fillColor = tinycolor("hsv(325, 100%, 100%)").toHexString();
+    this.path2.fillColor = tinycolor("hsv(325, 100%, 100%)").toHexString();
+    this.symbol = new Symbol(this.path),
+    this.symbol2 = new Symbol(this.path2),
+    this.startingX = 600,
+    this.startingY = 200,
+    this.startingY2 = 400,
+    this.startingPosition = new Point(this.startingX, this.startingY),
+    this.startingPosition2 = new Point(this.startingX, this.startingY2)
+}
+
+Obstacle.prototype = {
+    move: function(event){
         var aliveTime = event.time - this.spawnTime;
         this.newX = this.startingX - aliveTime*200;
         this.placedSymbol.position = new Point(this.newX, this.startingY);
         this.placedSymbol2.position = new Point(this.newX, this.startingY2);
-    };
-
-    Game.spawnCooldown = 0.5;
-
-    Game.player.jumping = false;
-    Game.player.startingX = 100; 
-    Game.player.startingY = 200; 
-    Game.player.startingPosition = new Point(Game.player.startingX, Game.player.startingY); 
-    Game.player.placedSymbol = Game.player.symbol.place(Game.player.startingPosition);
-    Game.player.startingX2 = 100; 
-    Game.player.startingY2 = 400; 
-    Game.player.startingPosition2 = new Point(Game.player.startingX2, Game.player.startingY2); 
-    Game.player.placedSymbol2 = Game.player.symbol2.place(Game.player.startingPosition2);
-    Game.things.push(Game.player);
-
-    view.onFrame = function(event) {
-        Game.timePlayed = event.time;
-        Game.moveThings(event);
-        Game.spawnThings(event);
     }
+}
 
-    function onMouseDown(event) {
-        if(!Game.player.jumping){
-            Game.player.startJump = true;
-        }
+function onMouseDown(event) {
+    if(!game.player.jumping){
+        game.player.startJump = true;
     }
+}
+
+window.onload = function() {
+    paper.setup('myCanvas');
     clicker = new Tool();
     clicker.onMouseDown = onMouseDown;
+    game = new Game();
 
+    view.onFrame = function(event) {
+        game.timePlayed = event.time;
+        game.moveThings(event);
+        game.spawnThings(event);
+    }
 }
+
